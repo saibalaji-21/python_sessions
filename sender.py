c24c67653d4e671bf044db6f1b3ade24cd1ca780
saibalaji-21 <147742483+saibalaji-21@users.noreply.github.com>
2024-11-29 18:52:12 +0530

    Add files via upload

---
+++ sender.py
@@ -0,0 +1,19 @@
+import socket
+s1=socket.socket(socket.AF_INET
+                 ,socket.SOCK_STREAM)
+s1.connect(("localhost",9999))
+data=input("Enter data to send : ")
+s1.send(bytes(data,"utf-8"))
+print("Data sent!!")
+responce=s1.recv(1024).decode()
+print(responce)
+s1.close()
+
+
+
+
+
+
+
+
+
